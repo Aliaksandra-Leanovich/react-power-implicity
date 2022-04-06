13c6291a4711abf9abe3d07a5040b847c11a1b29
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-04-06 12:22:27 +0300

    feat(nav): feat nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import "./App.scss";
+import Nav from "./components/Nav/Nav";
 
 const App = () => {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Nav />
+    </div>
+  );
 };
 export default App;
