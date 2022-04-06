dc5a27150cd0a6fb9e8632527551c6c01c814a83
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-04-06 12:32:11 +0300

    feat(header): feat header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Nav from "./components/Nav/Nav";
+import Header from "./components/Header/Header";
 
 const App = () => {
   return (
     <div className="app">
       <Nav />
+      <Header />
     </div>
   );
 };
