5c7d8336ea541fd50727e80b171c6e219ffb1035
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-04-07 12:04:02 +0300

    feat(header): add type to button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
           Instead of spending time searching for the right app, our AI will
           bring the right app to you.
         </p>
-        <button aria-label="learn" className="slider__button">
+        <button aria-label="learn" className="slider__button" type="button">
           Learn
         </button>
         <ol className="slider__list">
